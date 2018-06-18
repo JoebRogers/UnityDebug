b211259403a81e0b324088c9f66f1f77c42bf5a0
Joeb Rogers <joebrogers@pixcelstudios.uk>
2018-06-19 00:43:01 +0100

    Added script

---
+++ DebugUtility.cs
@@ -0,0 +1,172 @@
+﻿using UnityEngine;
+
+public static class DebugUtility
+{
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void Assert(bool condition)
+    {
+        Debug.Assert(condition);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void Assert(bool condition, Object context)
+    {
+        Debug.Assert(condition, context);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void Assert(bool condition, object message)
+    {
+        Debug.Assert(condition, message);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void Assert(bool condition, object message, Object context)
+    {
+        Debug.Assert(condition, message, context);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void AssertFormat(bool condition, string format, params object[] args)
+    {
+        Debug.AssertFormat(condition, format, args);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void AssertFormat(bool condition, Object context, string format, params object[] args)
+    {
+        Debug.AssertFormat(condition, context, format, args);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void Break()
+    {
+        Debug.Break();
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void ClearDeveloperConsole()
+    {
+        Debug.ClearDeveloperConsole();
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void DrawLine(Vector3 start, Vector3 end, Color color = default(Color), float duration = 0.0f, bool depthTest = true)
+    {
+        Debug.DrawLine(start, end, color, duration, depthTest);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void DrawRay(Vector3 start, Vector3 dir, Color color = default(Color), float duration = 0.0f, bool depthTest = true)
+    {
+        Debug.DrawRay(start, dir, color, duration, depthTest);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void Log(object message)
+    {
+        Debug.Log(message);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void Log(object message, Object context)
+    {
+        Debug.Log(message, context);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogAssertion(object message)
+    {
+        Debug.LogAssertion(message);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogAssertion(object message, Object context)
+    {
+        Debug.LogAssertion(message, context);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogAssertionFormat(string format, params object[] args)
+    {
+        Debug.LogAssertionFormat(format, args);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogAssertionFormat(Object context, string format, params object[] args)
+    {
+        Debug.LogAssertionFormat(context, format, args);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogError(object message)
+    {
+        Debug.LogError(message);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogError(object message, Object context)
+    {
+        Debug.LogError(message, context);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogErrorFormat(string format, params object[] args)
+    {
+        Debug.LogErrorFormat(format, args);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogErrorFormat(Object context, string format, params object[] args)
+    {
+        Debug.LogErrorFormat(context, format, args);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogException(System.Exception exception)
+    {
+        Debug.LogException(exception);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogException(System.Exception exception, Object context)
+    {
+        Debug.LogException(exception, context);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogFormat(string format, params object[] args)
+    {
+        Debug.LogFormat(format, args);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogFormat(Object context, string format, params object[] args)
+    {
+        Debug.LogFormat(context, format, args);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarning(object message)
+    {
+        Debug.LogWarning(message);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarning(object message, Object context)
+    {
+        Debug.LogWarning(message, context);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarningFormat(string format, params object[] args)
+    {
+        Debug.LogWarningFormat(format, args);
+    }
+
+    [System.Diagnostics.Conditional("DEVELOPMENT_BUILD"), System.Diagnostics.Conditional("UNITY_EDITOR")]
+    public static void LogWarningFormat(Object context, string format, params object[] args)
+    {
+        Debug.LogWarningFormat(context, format, args);
+    }
+}
